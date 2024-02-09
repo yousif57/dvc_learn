cdd1fb082cda2553ce8f3c874bdf928177f6a2b8
yousif57 <yousifk8780@gmail.com>
2024-02-09 18:11:59 +0200

    stage 1 complete

--- src/get_data.py
+++ src/get_data.py
@@ -10,12 +10,13 @@ def read_params(config_path):
         config = yaml.safe_load(yaml_file)
     return config
 
-def main(config_path, datasource):
+def get_data(config_path):
     config = read_params(config_path)
     #print(config)
     data_path = config["data_source"]["s3_source"]
     df = pd.read_csv(data_path, sep=",", encoding="utf-8")
-    print(df.head())
+    #print(df.head())
+    return df
 
 if __name__=="__main__":
     args = argparse.ArgumentParser()
@@ -24,4 +25,4 @@ if __name__=="__main__":
     args.add_argument("--datasource", default=None)
 
     parsed_args = args.parse_args()
-    main(config_path=parsed_args.config, datasource=parsed_args.datasource)
+    get_data(config_path=parsed_args.config)
