1529df895bcb449cef720c56bcf828b0a3c4c149
yousif57 <yousifk878@gmail.com>
2024-05-29 23:48:22 +0300

    little update

---
+++ test/conftest.py
@@ -0,0 +1,16 @@
+import yaml
+import pytest
+import os
+import json
+
+@pytest.fixture
+def config(config_path="params.yaml"):
+    with open(config_path) as yaml_file:
+        config = yaml.safe_load(yaml_file)
+    return config
+
+@pytest.fixture
+def get_schema(schema_path=schema_path):
+    with open(schema_path) as json_file:
+        schema = json.load(json_file)
+    return schema
